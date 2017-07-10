6a6c1cb7ed5a4ff2d983f4b8f068a966339e1c37
Jean <jean.sordes@yahoo.fr>
2017-07-10 23:19:54 +0200

    forgotten details in last commit

--- src/utility/db.php
+++ src/utility/db.php
@@ -6,7 +6,7 @@ class Db {
     public static function getPDO ($length = 18, $keyspace = ''){
 		$db_name = ($_SERVER["SERVER_NAME"] == 'tifod.com') ? 'tifod' : 'beta_tifod';
         $infos = [
-			'connect' => 'mysql:host=localhost;dbname=tifod;charset=utf8mb4',
+			'connect' => 'mysql:host=localhost;dbname=' . $db_name . ';charset=utf8mb4',
 			'user' => 'root',
 			'password' => ''
 		];
