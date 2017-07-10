fbf3326d833dc28287835fd866f6ca76e1e1cee8
Jean <jean.sordes@yahoo.fr>
2017-07-11 00:58:43 +0200

    db time zone trial

--- src/utility/db.php
+++ src/utility/db.php
@@ -14,6 +14,7 @@ class Db {
 		try {
             $db = new \PDO ($infos['connect'],$infos['user'],$infos['password']);
             $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+            $db->exec("SET SESSION time_zone = '+2:00'");
 		} catch(Exception $e) { throw $e; }
 		return $db;
     }
