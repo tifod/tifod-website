8366005601412eeb2f0e82838f3d93578cd20769
eurakilon <jean.sordes@yahoo.fr>
2017-07-27 11:20:09 +0200

    another test

--- index.php
+++ index.php
@@ -193,7 +193,8 @@ $app->post('/update-from-github', function ($request, $response, $args) {
     exec("rm -rf " . __DIR__ . "/src/templates/twig_cache/*");
     exec("touch " . __DIR__ . "/src/templates/twig_cache/.gitkeep");
     exec("php composer.phar update -o");
-    if (! $this->settings['displayErrorDetails']) file_put_contents(__DIR__ . "/version.txt",json_encode(json_encode($_POST['payload'])->release)->tag_name);
+    var_dump(json_encode($_POST['payload']));
+    // if (! $this->settings['displayErrorDetails']) file_put_contents(__DIR__ . "/version.txt",json_encode(json_encode($_POST['payload'])->release)->tag_name);
     return "<pre>" . $output . "</pre>";
 });
 $app->get('/get_last_posted_on/{project_id}/{last_time}', function ($request, $response, $args) {
