bd913780fd68ca7995f9d3426034eef25913e950
eurakilon <jean.sordes@yahoo.fr>
2017-10-01 23:31:25 +0200

    minor fix + error msg improved

--- update-from-github.php
+++ update-from-github.php
@@ -1,15 +1,20 @@
 <?php
+require_once __DIR__ . '/vendor/autoload.php';
+session_start();
+date_default_timezone_set('Europe/Paris');
+
 if (isset($_POST['payload'])){
     $db = MyApp\Utility\Db::getPDO();
-    $reponse = $db->query ('SELECT data_value FROM platform_data WHERE data_name = github_key');
-    $github_key = $reponse->fetch()['data_value'];
-    $reponse->closeCursor();
+    try {
+        $reponse = $db->query ('SELECT data_value FROM platform_data WHERE data_name = github_key');
+        $github_key = $reponse->fetch()['data_value'];
+        $reponse->closeCursor();
+    } catch (Throwable $e){
+        die ("platform_data.github_key is missing from the database, platform can't be updated");
+    }
     
     $header = getallheaders();
-    if (isset($header['X-Hub-Signature']) && $header['X-Hub-Signature'] === ('sha1=' . sha1($github_key)) ) {
-        require_once __DIR__ . '/vendor/autoload.php';
-        session_start();
-        date_default_timezone_set('Europe/Paris');
+    if (isset($header['X-Hub-Signature']) && $header['X-Hub-Signature'] === ('sha1=' . sha1($github_key))) {
 
         $result = [];
         $output = '';
@@ -19,10 +24,13 @@ if (isset($_POST['payload'])){
         exec("touch " . __DIR__ . "/src/templates/twig_cache/.gitkeep");
         exec("php composer.phar update -o");
         $version = ($_SERVER["SERVER_NAME"] != 'tifod.com') ? json_decode($_POST['payload'])->after : json_decode($_POST['payload'])->release->tag_name;
-        $db = MyApp\Utility\Db::getPDO();
         $reponse = $db->prepare ('UPDATE platform_data SET data_value = :version WHERE data_name =  version');
-        if (!$reponse->execute([ 'version' => $version ])) echo "platform_data.version is missing in database, can't be updated!";
-        $reponse->closeCursor();
+        try {
+            $reponse->execute([ 'version' => $version ]);
+            $reponse->closeCursor();
+        } catch (Throwable $e){
+            die ("platform_data.version is missing in database, can't be updated!");
+        }
         echo "<pre>" . $output . "</pre>";
     } else {
         echo "Signature not matching";
